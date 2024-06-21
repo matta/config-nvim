78a84f9a062aad3f2a95582a9d00b3305a1eb07e
Matt Armstrong <matt@rfc20.org>
2024-06-21 08:35:11 -0700

    Use \ as the prefix for window commands.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use \ as the introducer key for window operations. By default NeoVim
+-- uses <C-w> this, but that can be unwieldy.
+vim.keymap.set("n", "\\", "<C-w>", { desc = "Show Window menu", remap = true })
--- lua/plugins/extend-coding.lua
+++ lua/plugins/extend-coding.lua
@@ -6,16 +6,18 @@ return {
       local cmp = require("cmp")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         -- The basic problem is that using <CR> for accepting completion suggestions
-        -- sucks, because of the inherent ambiguity of pressing <CR> for the purpose
-        -- of actually entering that character in text. Completions can pop up
-        -- asynchronously and the typist has to deal with <CR> suddently changing
-        -- meaning, which is annoying.
+        -- is that it is annoying. There is an inherent ambiguity. Is the typist
+        -- pressing <CR> for the purpose of actually entering a carriage return,
+        -- or are they accepting a completion suggestion for a pop up that just
+        -- appeared?
         --
-        -- Do, take the suggeston from https://github.com/nvim-lua/kickstart.nvim
-        -- and bind <C-y> to accepting completion, and don't bind <CR> to anything.
+        -- So, take the suggeston from https://github.com/nvim-lua/kickstart.nvim
+        -- and bind <C-y> to accepting completion, and bind <CR> to doing what
+        -- it normally does.
         --
         -- Sadly, LazyVim binds <CR> to accepting the completion, so we must
-        -- rebind it to something else.
+        -- rebind it to a function that aborts completion and then attempts
+        -- to do whatever <CR> would have done had the popup not appeared.
         --
         -- Key bind inspiration from https://github.com/nvim-lua/kickstart.nvim/blob/5aeddfdd5d0308506ec63b0e4f8de33e2a39355f/init.lua#L732
         -- See also https://github.com/LazyVim/LazyVim/commit/e2972f0ccaf87c7d75bcd498fa9b2a64eb3d39a5#diff-16b34187d7492979aba539868172ff036dc83173b8e9078cd7989e949998f2a1
