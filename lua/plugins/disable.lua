3399a09c8a18522c0708cd2d8eaea2913f81013f
Matt Armstrong <matt@rfc20.org>
2024-06-21 08:05:36 -0700

    Disable bufferline.

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,4 +1,10 @@
 return {
+  -- I find that bufferline is redundant with ]b, [b,
+  -- and fuzzy finding by buffer name
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- I never found editor integrated file trees all that useful.
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- Vertical lines for indentation are noisy, and I do not
+  -- found them to be useful.
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
