57f9c7d5cd098acbbe56a01ba5daa67b8111f0c8
Matt Armstrong <matt@rfc20.org>
2024-06-24 16:29:05 -0700

    Disable inlay hints in insert mode

---
+++ plugin/disable-inlay-hint-in-insert-mode.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("InsertEnter", {
+  desc = "Disable lsp.inlay_hint when in insert mode",
+  callback = function(args)
+    local filter = { bufnr = args.buf }
+    local inlay_hint = vim.lsp.inlay_hint
+    if inlay_hint.is_enabled(filter) then
+      inlay_hint.enable(false, filter)
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        once = true,
+        desc = "Re-enable lsp.inlay_hint when leaving insert mode",
+        callback = function()
+          inlay_hint.enable(true, filter)
+        end,
+      })
+    end
+  end,
+})
